8e66022e17fdf3c25cc6669c81b416b7bd674378
Abu Zaher <azmfaridee@me.com>
2017-04-29 14:44:59 -0400

    pandas reading csv example

---
+++ read_csv.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pands as pd
+
+dfFaculties = pd.read_csv('crawled-data/faculty.csv', encoding='latin1')
+dfProjects = pd.read_csv('crawled-data/projects.csv', encoding='latin1')
