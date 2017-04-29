6b125f09bff8fed9f7d72a9b953bd330babbb4bf
Amrita Anam <amrita1@umbc.edu>
2017-04-29 19:44:11 -0400

    faculty terms updated

--- LSA/LSA.py
+++ LSA/LSA.py
@@ -131,6 +131,6 @@ def main(in_file, out_folder, save):
     # conceptTerms(terms, lsa)
 
 
-in_file = "/Users/amritaanam/Documents/GIT_Repo/hackumbc17/faculty.txt"
-out_folder = "/Users/amritaanam/Documents/GIT_Repo/hackumbc17/out_matrices/faculty/"
-main(in_file, out_folder, 1)
+in_file = "/Users/amritaanam/Documents/GIT_Repo/hackumbc17/projects.txt"
+out_folder = "/Users/amritaanam/Documents/GIT_Repo/hackumbc17/out_matrices/projects/"
+main(in_file, out_folder, 0)
