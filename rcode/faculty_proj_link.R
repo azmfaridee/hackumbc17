40b7de5df633c820aa69a6b472692058e1df2137
pooja01 <poojaparamesh@gmail.com>
2017-04-29 23:13:49 -0400

    update
    update

--- rcode/faculty_proj_link.R
+++ rcode/faculty_proj_link.R
@@ -1,17 +1,3 @@
-fid <- c(1,1,12,15)
-pid <- c(11,12,3,5)
-
-faculty_proj <- data.frame(fid,pid)
-
-
-sim <- c(0.9,0.7,0.6,0.5,0.45) 
-fid <- c(1,10,12,13,15)
-faculty_sim <- data.frame(sim,fid)
-
-
-sim <- c(0.6,0.56,0.5,0.45,0.4) 
-pid <- c(12,11,8,3,1)
-proj_sim <- data.frame(sim,pid)
 
 
 #for each faculty in the faculty_sim
