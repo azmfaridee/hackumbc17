9ca1daed8f607a4797c9dc500a46866e2690c91b
pooja01 <poojaparamesh@gmail.com>
2017-04-30 00:41:43 -0400

    update
    update

--- rcode/query_to_newfeaturespace_for_proj.R
+++ rcode/query_to_newfeaturespace_for_proj.R
@@ -9,7 +9,7 @@ library(tm)
 
 #query <- c('Stacy Branham is a Lecturer in the Department of Information Systems at UMBC. Dr. Branham\'s teaching focuses on making computing more appealing, accessible, and welcoming to first-year students towards increasing retention and diversity. Dr. Branham\'s research focuses on the role of computer technologies in mediating communication between significant others, friends, and colleagues. She works with populations that exhibit cross-cultural communication: people who are blind and people who are sighted; students and teachers; women and men. Through qualitative field studies and participant-observer engagements, she reflects on the design of technologies that can foster empathic communication--a form of talking that involves sharing, intimate connection, and mutual growth.')
 query <- c('My research interests include big data, workflow, distributed computing, user programming')
-#query <- c('privacy')
+#query <- c('health care')
 # build a corpus, and specify the source to be character vectors
 myCorpus <- Corpus(VectorSource(query))
 
